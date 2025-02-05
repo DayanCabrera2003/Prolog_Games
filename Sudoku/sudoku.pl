0ead8931fd7930babf01f1f88d1a0ed36f07f66a
Eveliz08 <espinaco.eveliz@gmail.com>
2025-02-04 21:04:29 -0500

    Add Sudoku constraint propagation and box extraction functions

--- Sudoku/sudoku.pl
+++ Sudoku/sudoku.pl
@@ -1,8 +1,68 @@
+removeItem(Item,List,Out) :-
+    select(Item,List,Out), !.
+removeItem(_,List,List).
+
+reduceDomain(_,[],[]) :- !.
+reduceDomain([],Domain,Domain) :- !.
+reduceDomain([H|T],Domain,Out) :- 
+    nonvar(H), removeItem(H,Domain,DomainWithoutValue), reduceDomain(T,DomainWithoutValue,Out), !.
+reduceDomain([H|T],Domain,Out) :- 
+    var(H), reduceDomain(T,Domain,Out).
+
+    obfuscateLine(_, [], []) :- !.
+obfuscateLine(Diff, [H|T], [H|ObfuscatedRest]) :-
+    random(X), X > Diff, !, obfuscateLine(Diff,T,ObfuscatedRest).
+obfuscateLine(Diff, [_|T], [_|ObfuscatedRest]) :-
+    obfuscateLine(Diff,T,ObfuscatedRest).
+
+    peelLeft([], [], []).
+peelLeft([[H|R]|T], [H|RestOfColumn], [R|RestOfRests]) :- peelLeft(T,RestOfColumn,RestOfRests).
+
+
+transpose([[]|_],[]) :- !.
+transpose(Matrix, [H|T]) :- peelLeft(Matrix,H,RestOfMatrix), transpose(RestOfMatrix,T).
+
+
+getBoxStack([],_,_,Buffer,[Buffer],[]) :- !.
+getBoxStack(Sudoku,Width,Height,Buffer,[Buffer|RestOfBoxes],RemainingRows):-
+    SizeOfBox is Width * Height,    
+    length(Buffer,SizeOfBox),
+    getBoxStack(Sudoku,Width,Height,[],RestOfBoxes,RemainingRows), !.
+getBoxStack([CurrentRow|Frontier],Width,Height,OldBuffer,Boxes,[RestOfCurrentRow|RemainingRows]):-
+    length(CurrentStackSlice, Width),
+    append(CurrentStackSlice, RestOfCurrentRow, CurrentRow),
+    append(CurrentStackSlice,OldBuffer,NewBuffer),
+    getBoxStack(Frontier,Width,Height,NewBuffer,Boxes,RemainingRows).
+
+
+    getBoxes_([],_,_,[]) :- !.
+getBoxes_([[]|_],_,_,[]) :- !.
+getBoxes_(Sudoku, Width, Height, Out) :-
+    getBoxStack(Sudoku,Width,Height,[],Stack,Rest),
+    append(Stack,OtherBoxes,Out),
+    getBoxes_(Rest,Width,Height,OtherBoxes).
+getBoxes([H|T], BoxWidth, BoxHeight, Out) :- 
+        length([H|T], MatrixSize), 
+        length(H, MatrixSize), 
+        Y is (MatrixSize mod BoxHeight),
+        X is (MatrixSize mod BoxWidth),
+        X = Y, Y = 0, BoxWidth \= 0, BoxHeight \= 0,
+        MatrixSize is BoxWidth * BoxHeight,
+        getBoxes_([H|T], BoxWidth, BoxHeight, Out)
+
 
 
 
 obtenerProblema(H, W, Filas,Salida) :-
     trasponer(Filas,Columnas), obtenerCajas(Filas,W,H,Cajas), append(Filas,Columnas,Temp), append(Temp,Cajas,Salida).
+
+    inequal(X,Y) :- X \== Y.
+allDifferent([]).
+allDifferent([X|Xss]) :- maplist(inequal(X), Xss), allDifferent(Xss).
+
+
+checkConsistency(Problem) :-
+    maplist(allDifferent, Problem).
 
 
 resolverPieza([], _, _, _) :- !.
